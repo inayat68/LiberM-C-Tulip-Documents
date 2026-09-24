2fb89045151e9427e99e0976def97b6e5263927d
Inayat Rehman <Inayat.Rehman@mlogica.com>
2026-09-24 22:54:36 +0500

    4th commit

--- aws_cards_demo_v1.8.4/Convert.bat
+++ aws_cards_demo_v1.8.4/Convert.bat
@@ -8,7 +8,7 @@
 :: The following license is ONLY for the mLogica team,
 :: do not share with anyone else
 ::
-set NIB_LICENSE_KEY=eyJ0eXAiOiJOSUItTElDRU5TRSIsImFsZyI6IkVTMjU2Iiwia2lkIjoibmliLWVzMjU2LXYxIn0.eyJsaWNlbnNlVmVyc2lvbiI6MywicHJvZHVjdE5hbWUiOiJOLkkuQi4iLCJ2ZW5kb3JOYW1lIjoiTklCIExhYnMgUy5yLmwuIiwiY3VzdG9tZXJOYW1lIjoiTklCIExhYnMgUy5yLmwuIiwiZXhwaXJlc09uIjoiMjAyNi0wOC0zMCIsImNyZWF0ZWRPbiI6IjIwMjYtMDgtMTEgMTY6MTM6MzgiLCJmZWF0dXJlcyI6eyJuaWIuY29tbW9ucyI6InllcyIsIm5pYi5zY3JpcHRpbmciOiJ5ZXMiLCJuaWIuc3VwZXJuYXV0IjoieWVzIiwibmliLm1vdG9yaGVhZCI6InllcyIsIm5pYi5hbmd1bGFyIjoieWVzIiwibmliLnRuMzI3MCI6InllcyIsIm5pYi5wYXJhbm9pZCI6InllcyJ9LCJwcm9wZXJ0aWVzIjpudWxsLCJwbGF0Zm9ybXMiOlsiSmF2YSIsIkRvdE5ldCJdfQ.ghbnFI1KqzrrkxTSeg6gWU8L77flvoVzELgZG04cQfDe_xWr6vMQSDv8uSGZM9JyzEjp91ubwxcq9GsQGiLF8A
+set NIB_LICENSE_KEY=eyJ0eXAiOiJOSUItTElDRU5TRSIsImFsZyI6IkVTMjU2Iiwia2lkIjoibmliLWVzMjU2LXYxIn0.eyJsaWNlbnNlVmVyc2lvbiI6MywicHJvZHVjdE5hbWUiOiJOLkkuQi4iLCJ2ZW5kb3JOYW1lIjoiTklCIExhYnMgUy5yLmwuIiwiY3VzdG9tZXJOYW1lIjoiTklCIExhYnMgUy5yLmwuIiwiZXhwaXJlc09uIjoiMjAyNi0wOS0zMCIsImNyZWF0ZWRPbiI6IjIwMjYtMDktMDEgMTc6Mzk6MjAiLCJmZWF0dXJlcyI6eyJuaWIuY29tbW9ucyI6InllcyIsIm5pYi5zY3JpcHRpbmciOiJ5ZXMiLCJuaWIuc3VwZXJuYXV0IjoieWVzIiwibmliLm1vdG9yaGVhZCI6InllcyIsIm5pYi5hbmd1bGFyIjoieWVzIiwibmliLnRuMzI3MCI6InllcyIsIm5pYi5wYXJhbm9pZCI6InllcyJ9LCJwcm9wZXJ0aWVzIjpudWxsLCJwbGF0Zm9ybXMiOlsiSmF2YSIsIkRvdE5ldCJdfQ.5XXqQWEvK9VF__iZMtaN-ZrZEF2jcajyn1YGm-PRvzFoCmQQUNsnwLLjCEUDgSsJQNeZnYA0uXMr_Mo5fZdiSA
 
 set CTOOL=nib-paranoid-1.8.4.exe
 
@@ -19,7 +19,7 @@ set DG_CONV_WORKSPACE_IN=%cd%
 ::
 set DG_CONV_WORKSPACE_OUT=%cd%\..\nib-java-demo-apps\aws-carddemo-app
 ::echo DG_CONV_WORKSPACE_OUT=%DG_CONV_WORKSPACE_OUT%
-::%CTOOL% conv_cobol_batch_java_config.xml
+%CTOOL% conv_cobol_batch_java_config.xml
 ::%CTOOL% conv_cobol_online_java_config.xml
 ::%CTOOL% conv_bms_angular_config.xml
 ::%CTOOL% conv_jcl_groovy_config.xml
@@ -28,11 +28,11 @@ set DG_CONV_WORKSPACE_OUT=%cd%\..\nib-java-demo-apps\aws-carddemo-app
 :: 
 ::  C# settings
 ::
-set DG_CONV_WORKSPACE_OUT=%cd%\..\nib-java-demo-apps-csharp
+::set DG_CONV_WORKSPACE_OUT=%cd%\..\nib-java-demo-apps-csharp
 ::echo DG_CONV_WORKSPACE_OUT=%DG_CONV_WORKSPACE_OUT%
-%CTOOL% conv_cobol_batch_c#_config.xml
-%CTOOL% conv_cobol_online_c#_config.xml
-%CTOOL% conv_jcl_pwsh_config.xml
-%CTOOL% conv_proc_pwsh_config.xml
+::%CTOOL% conv_cobol_batch_c#_config.xml
+::%CTOOL% conv_cobol_online_c#_config.xml
+::%CTOOL% conv_jcl_pwsh_config.xml
+::%CTOOL% conv_proc_pwsh_config.xml
 
 
